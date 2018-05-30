7bf0abd4a2ac69c866bf7de5561ef484a09e2f21
mat-der-D <pmahthyemsatics.14or0@gmail.com>
2018-05-31 02:12:02 +0900

    adv_up_down

---
+++ advection/downstream/advection_downstream.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class advection:
+    def __init__(self):
+        self.Lx = 50
+        self.Nx = 500
+        self.dt = 0.01
+        self.Nt = 500
+        self.dx = self.Lx / self.Nx
+        self.x = np.linspace(0, self.Lx, self.Nx, endpoint = False)
+        self.out = 10
+
+    def Next_u(self, u):
+        u_m = np.roll(u, +1)
+        du = (u - u_m)/(self.dx) * self.dt
+        return u + du
+
+adv = advection()
+u = np.sin(4*np.pi*adv.x / adv.Lx)
+
+f = open("adv.dat","w")
+for ix in range(adv.Nx):
+    f.write(str(adv.x[ix]) + " " + str(u[ix]) + "\n")
+
+f.write("\n\n")
+
+for it in range(adv.Nt + 1):
+    u = adv.Next_u(u)
+    if it % adv.out == 0:
+        for ix in range(adv.Nx):
+            f.write(str(adv.x[ix]) + " " + str(u[ix]) + "\n")
+
+        f.write("\n\n")
+
+f.close()
+
